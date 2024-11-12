9fb05cc56e2222b0954e70a2d61c322d7f92409c
Mathias Handeland <mathias.handeland@lyse.net>
2024-11-12 19:21:34 +0100

    added minutes left

--- code/src/main.js
+++ code/src/main.js
@@ -2,10 +2,12 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const minutesLeft = document.getElementById("minutes-left"); // New element
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
+  minutesLeft.innerText = getMinutesLeftOfYear(now); // Set minutes left
   timeContainer.innerText = getTimeString(now);
 }
 
